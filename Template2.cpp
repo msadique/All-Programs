1165248629c05440ae32036fea02ad810012e7d8
msadique <msadique@hawk.iit.edu>
2017-04-16 17:15:17 -0500

    some changes

--- Template2.cpp
+++ Template2.cpp
@@ -20,6 +20,7 @@ char printElement ()
 return element;
 }
 };
+hello
 You are given a main() function which takes a set of inputs. The type of input governs the kind of operation to be performed, i.e. concatenation for strings and addition for int or float. You need to write the class template AddElements which has a function add() for giving the sum of int or float elements. You also need to write a template specialization for the type string with a function concatenate() to concatenate the second string to the first string.
 
 Input Format
